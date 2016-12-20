b3d8bd261b7587054793019f77bb4dbc7a09b127
Mike Forgit <michael.forgit@wolterskluwer.com>
2016-12-19 20:55:34 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# gulp-bower-update
+Force updates to the latest version of your bower dependencies. It does this synchronously to allow for updates to 
+complete before executing dependent gulp tasks.
+
+Adapted from brunocartier's [bower-update-all](https://github.com/ActivKonnect/bower-update-all) to be used with gulp
+tasks.
+
+Important note, I wrote this to fit a need I had locally and this module probably could use to be extended. I did not
+intend to cover numerous use cases.
+
+Usage
+--------------
+
+```
+sudo npm install gulp-bower-update --save-dev
+```
+
+```
+var bowerUpdate = require('gulp-bower-update');
+
+gulp.task('gulp-bower-update', function () {
+    return gulp.src('bower.json')
+      .pipe(bowerUpdate(true))
+      .on('error', function (e) {
+         gulpUtil.log(e);
+       });
+});
+```
+
+After this, the dependencies in the provided bower.json file will be updated to their latest possible version.
+
+Parameters
+--------------
+    isGitOnly        Check only for dependencies that point to a .git location
---
+++ index.js
@@ -0,0 +1,126 @@
+'use strict';
+//Adapted from brunocartier's bower-update-all to be used with gulp
+var gutil = require('gulp-util');
+var execSync = require('child_process').execSync;
+var through = require('through2');
+var pluginName = 'bower-update-all';
+var pluginError = gutil.PluginError;
+
+//isGitOnly flag used to download from dependencies only in git repos
+/*jslint indent: 4, maxlen: 80, node: true */
+module.exports = function (isGitOnly) {
+    'use strict';
+    var DependencyUpdater;
+    var handleFile;
+
+    DependencyUpdater = function () {
+        var // Functions
+            updateDependency,
+            addDependency,
+            updateNext,
+
+            // Variables
+            self = {},
+            dependencies = [];
+
+        updateDependency = function (dep, cb) {
+            var command = 'bower install ' + dep.name + ' --force-latest --save';
+
+            if (dep.dev) {
+                command += '-dev';
+            }
+
+            var err = execSync(command);
+            if (err) {
+                console.log('Updated ' + dep.name + ' - ' + err);
+            }
+
+            cb();
+        };
+
+        addDependency = function (name, dev) {
+            dependencies.push({
+                name: name,
+                dev: !!dev
+            });
+        };
+
+        updateNext = function () {
+            if (dependencies.length) {
+                updateDependency(
+                    dependencies.shift(),
+                    updateNext
+                );
+            } else {
+                console.log('➤ All dependencies updated!');
+            }
+            return true;
+        };
+
+        self.addDependency = addDependency;
+        self.updateNext = updateNext;
+
+        return self;
+    };
+
+    handleFile = function (data) {
+        console.log('➤ Getting and parsing bower.json file...');
+
+        var bowerJson = JSON.parse(data),
+            updater = new DependencyUpdater(),
+            value = '';
+
+        if (bowerJson.dependencies) {
+            Object
+                .keys(bowerJson.dependencies)
+                .forEach(function (dep) {
+                    value = bowerJson.dependencies[dep];
+                    if(isGitOnly) {
+                        if(value.endsWith('git')) {
+                            updater.addDependency(value);
+                        }
+                    }
+                    else {
+                        updater.addDependency(value);
+                    }
+                });
+        }
+
+        if (bowerJson.devDependencies) {
+            Object
+                .keys(bowerJson.devDependencies)
+                .forEach(function (dep) {
+                    if(isGitOnly) {
+                        if(value.endsWith('git')) {
+                            updater.addDependency(value);
+                        }
+                    }
+                    else {
+                        updater.addDependency(value);
+                    }
+                });
+        }
+
+        return updater.updateNext();
+    };
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            console.log('✘ Problem getting your file, please try again.');
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new pluginError(pluginName, 'Streaming not supported'));
+            return;
+        }
+        if(!enc){
+            enc = 'utf8';
+        }
+        var content = file.contents.toString(enc);
+        handleFile(content);
+        //push back file to stream
+        cb(null, file);
+    });
+};
